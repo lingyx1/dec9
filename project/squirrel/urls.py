0a30097a984030725ef690acffdf1f912b57ec11
lingyx <yl4281@columbia.edu>
2019-12-09 05:54:47 +0000

    Export command, stats html

--- project/squirrel/urls.py
+++ project/squirrel/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
         path('', views.all_squirrels),
         path('add/', views.add_squirrel),
         path('<Unique_Squirrel_ID>/', views.edit_squirrel),
+        path('stats',views.stats_squirrel),
 ]
 
