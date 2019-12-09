8a1790711f4c098f815ed6a9b63c770e6eac3a67
Yuhan Huang <yh3221@columbia.edu>
2019-12-09 05:00:52 +0000

    map, sightings, edit, add part finished.

--- project/squirrel/views.py
+++ project/squirrel/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Squirrel
+from .forms import SquirrelForm
+  
+def all_squirrels(request):
+    sightings = Squirrel.objects.all()
+    context = {
+        'sightings': sightings,
+    }   
+    return render(request, 'squirrel/all.html', context)
+
+def edit_squirrel(request, Unique_Squirrel_ID):
+    squirrel = Squirrel.objects.get(Unique_Squirrel_ID=Unique_Squirrel_ID)
+    if request.method == 'POST':
+        # check form data
+        form = SquirrelForm(request.POST, instance=squirrel)
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings')
+    else:
+        form = SquirrelForm(instance=squirrel)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'squirrel/add.html', context)
+def add_squirrel(request):
+    if request.method == 'POST':
+        # check form data
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings')
+    else:
+        form = SquirrelForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'squirrel/add.html', context)
+
+def map(request):
+    sightings = Squirrel.objects.all()[0:99]
+    context = {
+        'sightings': sightings,
+    }
+    return render(request, 'squirrel/map.html', context)
+
